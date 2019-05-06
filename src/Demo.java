4d82b2b0274cfb20ce55416dd4fc3226f736807f
IRG <akashband7@gmail.com>
2019-05-06 15:47:14 +0530

    update code

--- src/Demo.java
+++ src/Demo.java
@@ -4,6 +4,7 @@ public class Demo
 	public static void main(String[] args) {
 		 System.out.println("Hello");
 		 System.out.println("bye");
+		 System.out.println("hi");
 		 int i =1;
 		 int j= 2;
 		 
